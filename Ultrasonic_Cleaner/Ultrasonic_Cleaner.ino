e513457fb5c2352cad59157eff503a6f2b1fc4a1
Anthony <m600x@users.noreply.github.com>
2017-05-18 15:12:02 +0200

    Reworked url handling, some UI change, Better time left

--- Ultrasonic_Cleaner/Ultrasonic_Cleaner.ino
+++ Ultrasonic_Cleaner/Ultrasonic_Cleaner.ino
@@ -63,23 +63,34 @@ void busy()
 {
 	String webBusy = "";
 	webBusy += "<div align=\"center\"><h1>Cleaning in progress</h1><h3>Heater is currently ";
-	webBusy += bool_heater ? "ON</h3>" : "OFF</h3>";
-	webBusy += "<a href=\"heat1\"><button>ON</button></a> <a href=\"heat0\"><button>OFF</button></a>";
+	webBusy += bool_heater ? "<strong><span style=\"color: #ff0000;\">ON</span></strong></h3>" : "OFF</h3>";
+	webBusy += "<a href=\"heat\"><button>TURN ON/OFF</button></a>";
 	webBusy += "<h3>Time left: ";
-	webBusy += String(cleantime / 60);
+	if ((cleantime / 60) > 60)
+	{
+		webBusy += String((cleantime / 60) / 60);
+		webBusy += " hours ";
+		webBusy += String((cleantime / 60) % 60);
+	}
+	else
+	{
+		webBusy += String(cleantime / 60);
+	}
 	webBusy += " minutes and ";
 	webBusy += String(cleantime % 60);
-	webBusy += " seconds.</h3><p><a href=\"emergency\"><button>EMERGENCY STOP</button></a></p></div>";
+	webBusy += " seconds.</h3><p><a href=\"stop\"><button>EMERGENCY STOP</button></a></p></div>";
+	webBusy += "&nbsp;<hr /><p style=\"text-align: center;\">";
+	webBusy += "<a href=\"https://github.com/m600x/WUC\">Source Code</a></p>";
 	server.send(200, "text/html", webBusy);
 }
 
 void normal()
 {
 	String webPage = "";
-	webPage += "<div align=\"center\"><h1>UltraSonic Cleaner</h1><h3>Heater is currently ";
-	webPage += bool_heater ? "ON</h3>" : "OFF</h3>";
-	webPage += "<a href=\"heat1\"><button>ON</button></a> <a href=\"heat0\"><button>OFF</button></a>";
-	webPage += "<h3>Timer in minutes</h3>";
+	webPage += "<div align=\"center\"><h1>Wireless Ultrasonic Cleaner</h1><h3>Heater is ";
+	webPage += bool_heater ? "<strong><span style=\"color: #ff0000;\">ON</span></strong></h3>" : "OFF</h3>";
+	webPage += "<a href=\"heat\"><button>TURN ON/OFF</button></a>";
+	webPage += "<h3>Timing</h3>";
 	webPage += "<a href=\"1\"><button>1:00</button></a> ";
 	webPage += "<a href=\"2\"><button>2:00</button></a> ";
 	webPage += "<a href=\"3\"><button>3:00</button></a> ";
@@ -90,25 +101,70 @@ void normal()
 	webPage += "<a href=\"30\"><button>30:00</button></a> ";
 	webPage += "<a href=\"40\"><button>40:00</button></a> ";
 	webPage += "<a href=\"50\"><button>50:00</button></a> ";
-	webPage += "<h3>Timer in hours</h3>";
-	webPage += "<a href=\"60\"><button>1:00</button></a> ";
-	webPage += "<a href=\"90\"><button>1:30</button></a> ";
-	webPage += "<a href=\"120\"><button>2:00</button></a> ";
-	webPage += "<a href=\"150\"><button>2:30</button></a> ";
-	webPage += "<a href=\"180\"><button>3:00</button></a> ";
-	webPage += "<a href=\"210\"><button>3:30</button></a> ";
-	webPage += "<a href=\"240\"><button>4:00</button></a> ";
-	webPage += "<a href=\"270\"><button>4:30</button></a> ";
-	webPage += "<a href=\"300\"><button>5:00</button></a> ";
-	webPage += "</div>";
+	webPage += "<p></p>";
+	webPage += "<a href=\"60\"><button>1 hour</button></a> ";
+	webPage += "<a href=\"120\"><button>2 hours</button></a> ";
+	webPage += "<a href=\"180\"><button>3 hours</button></a> ";
+	webPage += "<a href=\"240\"><button>4 hours</button></a> ";
+	webPage += "<a href=\"300\"><button>5 hours</button></a> ";
+	webPage += "<a href=\"360\"><button>6 hours</button></a> ";
+	webPage += "<a href=\"420\"><button>7 hours</button></a> ";
+	webPage += "<a href=\"480\"><button>8 hours</button></a> ";
+	webPage += "<a href=\"540\"><button>9 hours</button></a> ";
+	webPage += "<a href=\"600\"><button>10 hours</button></a> ";
+	webPage += "</div>&nbsp;<hr /><p style=\"text-align: center;\">";
+	webPage += "<a href=\"https://github.com/m600x/WUC\">Source Code</a></p>";
 	server.send(200, "text/html", webPage);
 }
 
-void dispatcher(int cltime)
+void dispatcher(int cltime, int heat, int stop)
 {
+	server.sendHeader("Location", String("/"), true);
+	server.send(302, "text/plain", "");
 	bool_running ? busy() : normal();
-	if (!bool_running)
+	if (cltime && !bool_running)
 		cleaning(cltime);
+	if (heat)
+	{
+		if (!bool_heater && pusher(BTN_HEAT))
+			bool_heater = 1;
+		else if (bool_heater && pusher(BTN_HEAT))
+			bool_heater = 0;
+	}
+	if (stop)
+	{
+		if (bool_heater && pusher(BTN_HEAT))
+			bool_heater = 0;
+		if (bool_running)
+			bool_running = 0;
+	}
+}
+
+void setserver()
+{
+	server.on("/", [](){ bool_running ? busy() : normal();;});
+	server.on("/heat", [](){ dispatcher(0, 1, 0);});
+	server.on("/stop", [](){ dispatcher(0, 0, 1);});
+	server.on("/1", [](){ dispatcher(1, 0, 0);});
+	server.on("/2", [](){ dispatcher(2, 0, 0);});
+	server.on("/3", [](){ dispatcher(3, 0, 0);});
+	server.on("/4", [](){ dispatcher(4, 0, 0);});
+	server.on("/5", [](){ dispatcher(5, 0, 0);});
+	server.on("/10", [](){ dispatcher(10, 0, 0);});
+	server.on("/20", [](){ dispatcher(20, 0, 0);});
+	server.on("/30", [](){ dispatcher(30, 0, 0);});
+	server.on("/40", [](){ dispatcher(40, 0, 0);});
+	server.on("/50", [](){ dispatcher(50, 0, 0);});
+	server.on("/60", [](){ dispatcher(60, 0, 0);});
+	server.on("/120", [](){ dispatcher(120, 0, 0);});
+	server.on("/180", [](){ dispatcher(180, 0, 0);});
+	server.on("/240", [](){ dispatcher(240, 0, 0);});
+	server.on("/300", [](){ dispatcher(300, 0, 0);});
+	server.on("/360", [](){ dispatcher(360, 0, 0);});
+	server.on("/420", [](){ dispatcher(420, 0, 0);});
+	server.on("/480", [](){ dispatcher(480, 0, 0);});
+	server.on("/540", [](){ dispatcher(540, 0, 0);});
+	server.on("/600", [](){ dispatcher(600, 0, 0);});
 }
 
 void setup()
@@ -128,45 +184,7 @@ void setup()
 		delay(500);
 	digitalWrite(LED_BUILTIN, LOW);
 	mdns.begin("esp8266", WiFi.localIP());
-	server.on("/", [](){
-		bool_running ? busy() : normal();
-	});
-	server.on("/heat1", [](){
-		bool_running ? busy() : normal();
-		if (!bool_heater && pusher(BTN_HEAT))
-			bool_heater = 1;
-	});
-	server.on("/heat0", [](){
-		bool_running ? busy() : normal();
-		if (bool_heater && pusher(BTN_HEAT))
-			bool_heater = 0;
-	});
-	server.on("/emergency", [](){
-		if (bool_heater && pusher(BTN_HEAT))
-			bool_heater = 0;
-		if (bool_running)
-			bool_running = 0;
-		bool_running ? busy() : normal();
-	});
-	server.on("/1", [](){ dispatcher(1);});
-	server.on("/2", [](){ dispatcher(2);});
-	server.on("/3", [](){ dispatcher(3);});
-	server.on("/4", [](){ dispatcher(4);});
-	server.on("/5", [](){ dispatcher(5);});
-	server.on("/10", [](){ dispatcher(10);});
-	server.on("/20", [](){ dispatcher(20);});
-	server.on("/30", [](){ dispatcher(30);});
-	server.on("/40", [](){ dispatcher(40);});
-	server.on("/50", [](){ dispatcher(50);});
-	server.on("/60", [](){ dispatcher(60);});
-	server.on("/90", [](){ dispatcher(90);});
-	server.on("/120", [](){ dispatcher(120);});
-	server.on("/150", [](){ dispatcher(150);});
-	server.on("/180", [](){ dispatcher(180);});
-	server.on("/210", [](){ dispatcher(210);});
-	server.on("/240", [](){ dispatcher(240);});
-	server.on("/270", [](){ dispatcher(270);});
-	server.on("/300", [](){ dispatcher(300);});
+	setserver();
 	server.begin();
 }
 
